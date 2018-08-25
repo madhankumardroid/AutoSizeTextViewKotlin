59d2311c073f620b2febdc2dcfd17ee2c81e169f
Madhan <madhan@mallow-tech.com>
2018-08-25 22:07:50 +0530

    Initial Commit
    Initial commit.

---
+++ AutoSizeTextViewKotlin/app/src/main/java/com/example/madhan/autosizetextviewkotlin/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.madhan.autosizetextviewkotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ed_content.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val content = s.toString()
+                tv_auto_size.text = content
+                tv_auto_preset_size.text = content
+            }
+        })
+    }
+}
